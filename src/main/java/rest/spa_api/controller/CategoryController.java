6cf5e9f85190f3cda05358cb94a635b9fe36ede4
Serhii Beseda <sergaandd@gmail.com>
2023-03-19 08:47:17 +0200

    +Rest origin

--- src/main/java/rest/spa_api/controller/CategoryController.java
+++ src/main/java/rest/spa_api/controller/CategoryController.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,6 +19,7 @@ import rest.spa_api.service.CategoryService;
 
   @RestController
   @RequestMapping("/category")
+  @CrossOrigin(origins="http://localhost:4200")
   public class CategoryController {
 
   @Autowired
